c288b81a163429e50a083df6ded69b03e4d1efca
CodeWithMU <110249826+CodeWithMU@users.noreply.github.com>
2022-07-29 22:01:19 +0500

    Add files via upload

---
+++ lecture_5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 4;
+    int b = 5;
+    int sum = a + b;
+    int sub = a - b;
+    cout << sum << endl;
+    cout << sub << endl;
+    cout << 4 % 5 * 6 / 3 + 3 - 6 << endl; // 4*6/3+3-6->4*2+3-6->8+3-6=5
+    return 0;
+}
